e79516d1b42b3a9754b36ad58a9daf3c1e004c21
Kamil Preis <preiskamil@gmail.com>
2018-07-01 21:55:00 +0200

    popular films category

---
+++ app/src/main/java/com/ydubinskyi/filmsapp/PopularFilmsActivity.java
@@ -0,0 +1,49 @@
+package com.ydubinskyi.filmsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.ydubinskyi.filmsapp.adapter.MoviesAdapter;
+import com.ydubinskyi.filmsapp.api.ApiService;
+import com.ydubinskyi.filmsapp.api.Movie;
+import com.ydubinskyi.filmsapp.api.MovieResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class PopularFilmsActivity extends AppCompatActivity {
+
+    private final static String API_KEY = "24887d21d7f4a96154895b95c9723021";
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popular_films);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        Call<MovieResponse> topMoviesCall = ApiService.getService().getPopularMovies(API_KEY);
+
+        topMoviesCall.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                List<Movie> movies = response.body().getResults();
+                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+                Log.d("TAG", t.toString());
+            }
+        });
+    }
+}
